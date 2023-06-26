f191262ef1de723225933ef5cdfb5ba08026c480
moracabanas <moracabanas@live.com>
2023-06-26 13:04:49 +0200

    Update

--- perros.py
+++ perros.py
@@ -1,12 +1,16 @@
-class Perro():
+class Perro:
+    def __init__(self, nombre, raza, edad=0):
+        self.nombre = nombre + nombre
+        self.raza = raza
+        self.edad = edad
 
-  def __init__(self, nombre, raza, edad):
-    self.nombre = nombre
-    self.raza = raza
-    self.edad = edad
-  def ladra(self):
-    print(self.nombre, "dice 'Wooof!'")
-    
-perro1 = Perro("Rex", "Chihuahua", 2)
+    def ladra(self):
+        print(self.nombre, "dice 'Wooof!'")
 
-perro1.ladra()
+    def saludar():
+        print("Hola soy de la raza", self.raza)
+
+
+perro1 = Perro("Rex", "Chihuahua")
+
+perro1.saludar()  # TODO why Perro.saludar() takes 0 positional arguments but 1 was given
